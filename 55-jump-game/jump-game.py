045f40b9301aafb99491580c0090547c280d0e0b
Kiran Babu <kiranbabu3355@gmail.com>
2024-09-17 23:10:48 +0530

    Time: 768 ms (11.60%) | Memory: 12.7 MB (74.28%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,11 +1,12 @@
 class Solution(object):
     def canJump(self, nums):
-        ans=nums[0]
-        for i in range(len(nums)):
+        ans=0
+        n=len(nums)
+        for i in range(n):
             if i>ans:
                 return False
             ans=max(ans,i+nums[i])
-            if ans>=len(nums)-1:
+            if ans>=n-1:
                 return True
         return True
             
