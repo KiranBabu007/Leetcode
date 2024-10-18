60d441b0d8668f48a003c58642ec3ff34f9bd9d0
Kiran Babu <kiranbabu3355@gmail.com>
2024-10-18 23:53:34 +0530

    Time: 1 ms (100.00%) | Memory: 11.8 MB (99.25%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,11 +1,15 @@
 class Solution(object):
     def searchMatrix(self, matrix, target):
-        for i in matrix:
-            if target in i:
-                return True
-        
+        for nums in matrix:
+            low,high=0,len(nums)-1
+
+            while(low<=high):
+                mid=(low+high)//2
+
+                if target==nums[mid]:
+                    return True
+                if nums[mid]<target:
+                    low=mid+1
+                else:
+                    high=mid-1
         return False
-
-
-
-        
