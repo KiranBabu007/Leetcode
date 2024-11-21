a06d2cf8e2659d4dd4b35855e3e04750b5a186b8
Kiran Babu <kiranbabu3355@gmail.com>
2024-11-21 20:39:52 +0530

    Time: 4 ms (18.88%) | Memory: 13.1 MB (5.00%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isPerfectSquare(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+
+        for i in range(1,int(sqrt(num))+1):
+            if i*i==num:
+                return True
+        return False
+        
