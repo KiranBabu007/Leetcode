46bb8b78274573a43e6d6babda11ebe128ef0a94
Kiran Babu <kiranbabu3355@gmail.com>
2024-12-03 23:24:15 +0530

    Time: 16 ms (63.61%) | Memory: 12.3 MB (6.26%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,24 +1,16 @@
 class Solution(object):
     def combinationSum(self, candidates, target):   
         def findcomb(i,arr,t,st,ans):
-
             if t==0:
                 ans.append(list(st))
                 return
-
             if i==len(arr):
-                return
-            
+                return   
             if arr[i]<=t:
                 st.append(arr[i])
                 findcomb(i,arr,t - arr[i],st,ans)
                 st.pop()
             findcomb(i+1,arr,t,st,ans)
-
         ans=[]
         findcomb(0,candidates,target,[],ans)
-
         return ans
-
-            
-        
