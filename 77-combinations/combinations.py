d3cb468568f8978c1a4d682a6be0eee3882ce04e
Kiran Babu <kiranbabu3355@gmail.com>
2024-12-08 18:44:08 +0530

    Time: 459 ms (9.68%) | Memory: 131.4 MB (20.15%) - LeetSync

--- 77-combinations/combinations.py
+++ 77-combinations/combinations.py
@@ -5,15 +5,15 @@ class Solution(object):
         :type k: int
         :rtype: List[List[int]]
         """
-        def findComb(ind,n,k,path,ans):
+        def findComb(ind,path):
             if len(path)==k:
                 ans.append(path[:])
             for i in range(ind,n+1):
                 path.append(i)
-                findComb(i+1,n,k,path,ans)
+                findComb(i+1,path)
                 path.pop()
         ans=[]
-        findComb(1,n,k,[],ans)
+        findComb(1,[])
        
         return ans
         
