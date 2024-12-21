35d5488005bcb9d0a1bbed3a9ff212655a5d6bce
Kiran Babu <kiranbabu3355@gmail.com>
2024-12-21 23:09:06 +0530

    Time: 1528 ms (5.00%) | Memory: 31.3 MB (14.20%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,33 +1,31 @@
 class Solution(object):
     def numIslands(self, grid):
         if not grid:
-            return
-        island=0
-        vis=set()
-        rows,cols=len(grid),len(grid[0])
+            return 
+
+        rows=len(grid)
+        cols=len(grid[0])
+        islands=0
+        visited=set()
 
         def bfs(r,c):
             q=collections.deque()
-            vis.add((r,c))
+            directions=[[0,-1],[0,1],[1,0],[-1,0]]
             q.append((r,c))
-            while(q):
+            visited.add((r,c))
+            while q:
                 row,col=q.popleft()
-                directions=[[0,-1],[0,1],[1,0],[-1,0]]
                 for dr,dc in directions:
                     r,c=row+dr,col+dc
-                    if r in range(rows) and c in range(cols) and (r,c) not in vis and grid[r][c]=="1":
-                        vis.add((r,c))
+                    if (r in range(rows) and c in range(cols) and grid[r][c]=='1' and (r,c) not in visited):
                         q.append((r,c))
-
-
-                
-
-
+                        visited.add((r,c))
+     
         for i in range(rows):
             for j in range(cols):
-                if grid[i][j]=="1" and (i,j) not in vis:
+                if grid[i][j]=='1' and (i,j) not in visited:
                     bfs(i,j)
-                    island+=1
-        return island
+                    islands+=1
+        return islands
 
         
