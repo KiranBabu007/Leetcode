dc02258921b51ff32040db7c90db786c4f8cd0e8
Kiran Babu <kiranbabu3355@gmail.com>
2024-12-22 00:08:17 +0530

    Time: 188 ms (17.30%) | Memory: 26.1 MB (5.14%) - LeetSync

--- 463-island-perimeter/island-perimeter.py
+++ 463-island-perimeter/island-perimeter.py
@@ -6,17 +6,23 @@ class Solution(object):
         """
         r=len(grid)
         c=len(grid[0])
-        p=0
-        d=[[0,1],[0,-1],[1,0],[-1,0]]      
+        visit=set()
+        def dfs(row,col):
+            if (0>row or 0>col or row>=r or col>=c or grid[row][col]==0):
+                return 1
+            if (row,col) in visit:
+                return 0
+            visit.add((row,col))
+            p=dfs(row,col+1)
+            p+=dfs(row,col-1)
+            p+=dfs(row+1,col)
+            p+=dfs(row-1,col)
+            return p
+
         for i in range(r):
             for j in range(c):  
                 if(grid[i][j]==1):
-                    p+=4
-                    for dr,dc in d:
-                        newr,newc=i+dr,j+dc
-                        if(newr in range(r) and newc in range(c) and grid[newr][newc]==1):
-                            p-=1
-        
+                    return dfs(i,j)
         return p
 
 
