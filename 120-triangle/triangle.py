2a962c7414de20cc54e83cc71cc80a4eeffe0278
Kiran Babu <kiranbabu3355@gmail.com>
2025-01-02 00:02:55 +0530

    Time: 3 ms (86.87%) | Memory: 13.1 MB (29.61%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -4,9 +4,9 @@ class Solution(object):
         :type triangle: List[List[int]]
         :rtype: int
         """
-        dp=[0] * (len(triangle)+1)
+        dp=triangle[-1]
 
-        for row in triangle[::-1]:
+        for row in triangle[:-1][::-1]:
             for i,n in enumerate(row):
                 dp[i]=n+min(dp[i],dp[i+1])
         return dp[0]
