79a2119e115269d8d9c546579daa2e82d41e8a7e
Kiran Babu <kiranbabu3355@gmail.com>
2025-01-17 23:51:27 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (25.44%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        stack=[]
+        res=[]
+
+        def backtrack(l,r):
+            if l==r==n:
+                res.append("".join(stack))
+                return
+            if l<n:
+                stack.append('(')
+                backtrack(l+1,r)
+                stack.pop()
+            if r<l:
+                stack.append(')')
+                backtrack(l,r+1)
+                stack.pop()
+
+        backtrack(0,0)
+        return res
+        
