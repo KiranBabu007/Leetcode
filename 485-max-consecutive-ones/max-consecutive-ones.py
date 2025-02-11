b7b5be8a3e35a19cc4f12137d844ec521f26c2a8
Kiran Babu <kiranbabu3355@gmail.com>
2025-02-11 11:09:50 +0530

    Time: 50 ms (5.43%) | Memory: 16.3 MB (18.65%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.py
+++ 485-max-consecutive-ones/max-consecutive-ones.py
@@ -7,8 +7,9 @@ class Solution(object):
         l=0
         ans=0
         for r in range(len(nums)):
-            if nums[r]==0:
+            if nums[r]==0:   
                 l=r+1
+                
             ans=max(ans,r-l+1)
         return ans
             
