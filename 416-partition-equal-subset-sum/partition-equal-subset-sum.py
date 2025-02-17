713735725ea2a58e6ff2b8dac3f5c93e472f0f88
Kiran Babu <kiranbabu3355@gmail.com>
2025-02-17 21:02:43 +0530

    Time: 271 ms (95.36%) | Memory: 14.1 MB (55.22%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -4,24 +4,22 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        memo={}
-        def helper(n,i):
-            if (n,i) in memo:
-                return memo[(n,i)]
-            if n==0:
-                return True
-            if n<0 or i>=len(nums): 
-                return False
-            memo[(n,i)]=helper(n-nums[i],i+1) or helper(n,i+1)
-            return memo[(n,i)]
-            
-        t=sum(nums)
-
-        if t%2!=0:
+        total=sum(nums)
+        if total%2!=0:
             return False
 
-        target=t//2
-    
-        return helper(target,0)
+        target=total/2
+
+        dp=set([0])
+
+        for n in nums:
+            l=set()
+            for i in dp:
+                if i+n==target:
+                    return True
+                l.add(i+n)
+            dp.update(l)
+        return False
+                
 
         
