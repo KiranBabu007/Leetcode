ff3c6b207d494a558db0040127361069efc66697
Kiran Babu <kiranbabu3355@gmail.com>
2025-02-17 19:09:43 +0530

    Time: 2064 ms (33.66%) | Memory: 172.2 MB (7.74%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -12,9 +12,8 @@ class Solution(object):
                 return True
             if n<0 or i>=len(nums): 
                 return False
-            memo[(n-nums[i],i+1)]=helper(n-nums[i],i+1)
-            memo[(n,i+1)]=helper(n,i+1)
-            return memo[(n-nums[i],i+1)] or memo[(n,i+1)]
+            memo[(n,i)]=helper(n-nums[i],i+1) or helper(n,i+1)
+            return memo[(n,i)]
             
         t=sum(nums)
 
