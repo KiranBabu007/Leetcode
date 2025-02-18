eb67cd276705cab4c02a5c866421afaf4b656694
Kiran Babu <kiranbabu3355@gmail.com>
2025-02-18 12:35:03 +0530

    Time: 240 ms (48.89%) | Memory: 31.3 MB (30.98%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -14,15 +14,18 @@ class Solution(object):
             while q:
                 row,col=q.popleft()
                 for dr,dc in directions:
-                    if 0<=dr+row<rows and 0<=dc+col<cols and grid[dr+row][dc+col]=="1":
-                        q.append((dr+row,dc+col))
-                        grid[dr+row][dc+col]=0
-     
+                    new_row,new_col=dr+row,dc+col
+                    if 0<=new_row<rows and 0<=new_col<cols and grid[new_row][new_col]=="1" and (new_row,new_col) not in visited:
+                        q.append((new_row,new_col))
+                        visited.add((new_row,new_col))
+                        
+        visited=set()
         count=0
         for i in range(rows):
             for j in range(cols):
-                if grid[i][j]=="1":
+                if grid[i][j]=="1" and (i,j) not in visited:
                     bfs(i,j)
+                    visited.add((i,j))
                     count+=1
         return count
 
