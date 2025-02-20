21ccec76d77e5b299a1d17033dbe0e1fb7205a30
Kiran Babu <kiranbabu3355@gmail.com>
2025-02-20 22:55:40 +0530

    Time: 17 ms (29.21%) | Memory: 12.9 MB (78.53%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def findCircleNum(self, isConnected):
+        """
+        :type isConnected: List[List[int]]
+        :rtype: int
+        """
+        n=len(isConnected)
+        p=[i for i in range(n)]
+        rank=[1] * n
+
+        def find(node):
+            while node != p[node]:
+                node=p[node]
+            return node
+
+        def union(n1,n2):
+            p1,p2=find(n1),find(n2)
+
+            if p1==p2:
+                return 0
+            if rank[p2]>rank[p1]:
+                p[p1]=p2
+                rank[p2]+=rank[p1]
+            else:
+                p[p2]=p1
+                rank[p1]+=rank[p2]
+            return 1
+
+        ans=n
+        for i in range(n):
+            for j in range(n):
+                if isConnected[i][j] == 1:  
+                    ans -= union(i, j)
+        return ans
+        
