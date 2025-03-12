6b5bfd7bf9be3348ec06b4699a1cedf8ccadd401
Kiran Babu <kiranbabu3355@gmail.com>
2025-03-12 23:11:24 +0530

    Time: 81 ms (89.51%) | Memory: 20.8 MB (55.36%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -4,19 +4,16 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        
-        n=len(height)
-        l,r,res=0,n-1,0
-
+        l=0
+        r=len(height)-1
+        ans=0
         while(l<r):
-            area=(r-l)*min(height[l],height[r])
-            res=max(res,area)
-
-            if(height[l]<=height[r]):
+            if min(height[l],height[r])*(r-l) > ans:
+                ans=min(height[l],height[r])*(r-l)
+            if height[l]<height[r]:
                 l+=1
             else:
                 r-=1
-        return res
-
-
+        return ans
+            
         
