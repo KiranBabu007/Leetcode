91536ff063291d7b2e95a01548317acdc6e72d2a
Kiran Babu <kiranbabu3355@gmail.com>
2025-03-12 20:38:39 +0530

    Time: 7 ms (9.74%) | Memory: 13.2 MB (82.90%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -9,22 +9,17 @@ class Solution(object):
         for i in prices: 
             if not q:
                 q.append(i)
-                print(q)
-                continue
-            if q[-1]<i:
+            elif q[-1]<i:
                 q.append(i)
             else:
                 if len(q)==1:
                     q.pop()
-                    q.append(i)
-                    print(q)
-                    continue
-                print("empty akanam")
-                profit+=(q.pop()-q.popleft())
-                while q:
-                    q.pop()
+                else:
+                    profit+=(q.pop()-q.popleft())
+                    while q:
+                        q.pop()
                 q.append(i)
-            print(q)
+            
         if len(q)>1:
             profit+=(q.pop()-q.popleft())
         return profit
